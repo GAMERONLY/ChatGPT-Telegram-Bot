dd3ad72b28039061e31ae81341eeff80a476adf5
GAMERONLY <81401125+GAMERONLY@users.noreply.github.com>
2023-10-09 15:24:09 +0530

    Update harshitethic.py

--- harshitethic.py
+++ harshitethic.py
@@ -7,7 +7,7 @@ import openai
 #OpenAI API key
 aienv = os.getenv('OPENAI_KEY')
 if aienv == None:
-    openai.api_key = "ENTER YOUR API KEY HERE"
+    openai.api_key = "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"
 else:
     openai.api_key = aienv
 print(aienv)
@@ -15,7 +15,7 @@ print(aienv)
 #Telegram bot key
 tgenv = os.getenv('TELEGRAM_KEY')
 if tgenv == None:
-    tgkey = "ENTER YOUR TELEGRAM TOKEN HERE"
+    tgkey = "5846752699:AAHxobSEQq5rshbZaRYzTLN6W_OK0iHja6Y"
 else:
     tgkey = tgenv
 print(tgenv)
